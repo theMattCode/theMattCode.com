29891cd096af620954b719e7f8686efdf73c759d
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-05-23 13:45:18 +0200

    Remove all stylings

--- src/components/Layout/FullScreen.tsx
+++ src/components/Layout/FullScreen.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { createUseStyles } from 'react-jss';
 import { SEO } from '../SEO';
-
-const useStyles = createUseStyles({
-  main: {
-    display: 'grid',
-    alignContent: 'center',
-    textAlign: 'center',
-    margin: '0 auto',
-    minHeight: '100vh'
-  }
-});
 
 export const FullScreen: React.FC = ({ children }) => {
   return (
     <>
       <SEO />
-      <main className={useStyles().main}>{children}</main>
+      <main>{children}</main>
     </>
   );
 };
