0e0605532ff5db2f9ca39b5bd8996b30a4092901
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-05-23 12:39:28 +0200

    Just rearrange code

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -2,13 +2,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-export const SEO: React.FC = () => {
-  const data = useStaticQuery(query);
-  const { title } = data.site.siteMetadata;
-  return <Helmet title={title} />;
-};
-
-export const query = graphql`
+const query = graphql`
   query SEOQuery {
     site {
       siteMetadata {
@@ -17,3 +11,9 @@ export const query = graphql`
     }
   }
 `;
+
+export const SEO: React.FC = () => {
+  const data = useStaticQuery(query);
+  const { title } = data.site.siteMetadata;
+  return <Helmet title={title} />;
+};
