41abebeb6fcf7e0beb0c43c5a1b42b81fd3a9a42
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2021-07-22 10:09:17 +0200

    Ignore cypress specs for jest test runs
    gh-15

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.[jt]sx?$': '<rootDir>/.jest/preprocess.js',
   },
+  modulePathIgnorePatterns: ['<rootDir>/cypress/'],
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/.jest/file-mock.js`,
